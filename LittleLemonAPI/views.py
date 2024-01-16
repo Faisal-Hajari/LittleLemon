fa47f6a47bb87c3153d36b86be3f91c24be20667
faisalhajary <faisalhejary@gmail.com>
2024-01-16 12:15:31 +0300

    fixed generic list import

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -2,6 +2,6 @@ from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializers
 
-class MenuItemView(generics.ListAPIView): 
+class MenuItemView(generics.ListCreateAPIView): 
     queryset = MenuItem.objects.all() 
     serializer_class = MenuItemSerializers
